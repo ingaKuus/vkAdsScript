320aa52964b973c4874870f96aaed6d3e8cf5aae
ingaKuus <38397419+ingaKuus@users.noreply.github.com>
2026-07-31 22:04:53 +0300

    Update main.user.js

--- main.user.js
+++ main.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         VkAds_RIP
 // @namespace    http://tampermonkey.net/
-// @version      1.3.1
+// @version      1.3.2
 // @description  hides ads on VK.com
 // @author       theUniverse
 // @match        https://vk.com/*
